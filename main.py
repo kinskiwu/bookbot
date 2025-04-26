b60fa07a108ebee3a4da2d3ec20b481c36fbf7c6
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2025-04-26 11:08:30 -0500

    feature: count num words

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def main() -> None:
   book_path = 'books/frankenstein.txt'
   text = get_book_text(book_path)
-  print(text)
+  num_words = count_num_words(text)
+  # print(text)
+  print(f"{num_words} words found in the document")
 
 def get_book_text(filepath: str) -> str:
   with open(filepath) as f:
     return f.read()
+
+def count_num_words(book_text: str) -> int:
+  temp = book_text.split()
+  return len(temp)
 
 main()
