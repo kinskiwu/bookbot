71577ba073453dff1f30d6b61754a01db4290ed6
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2025-04-26 11:01:15 -0500

    feature: read file content

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main() -> None:
+  book_path = 'books/frankenstein.txt'
+  text = get_book_text(book_path)
+  print(text)
+
+def get_book_text(filepath: str) -> str:
+  with open(filepath) as f:
+    return f.read()
+
+main()
