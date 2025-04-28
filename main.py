81ca10d3491684a8ff0835b5a5f88a321c35523f
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2025-04-28 07:29:23 -0500

    feature: count chars

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from stats import count_num_words
+from stats import count_num_words, count_chars
 
 def main() -> None:
   book_path = 'books/frankenstein.txt'
   text = get_book_text(book_path)
   num_words = count_num_words(text)
+  char_count_dict = count_chars(text)
   # print(text)
-  print(f"{num_words} words found in the document")
+  print(f"{num_words} words found in the document", char_count_dict)
 
 def get_book_text(filepath: str) -> str:
   with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,22 @@
 def count_num_words(book_text: str) -> int:
+  if not book_text:
+    return 0
   temp = book_text.split()
   return len(temp)
+
+def count_chars(book_text: str) -> dict:
+  if not book_text:
+    return {}
+
+  char_count = {}
+
+  for char in book_text:
+    formatted_char = char.lower()
+    if formatted_char not in char_count:
+      char_count[formatted_char] = 1
+    else:
+      char_count[formatted_char] += 1
+
+  return char_count
+
+
