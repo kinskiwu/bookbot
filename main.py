c21148be9286e02ebee47d78ac8348df4e09a592
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2025-04-27 22:03:23 -0500

    refactor: move helper ful to stats

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_num_words
+
 def main() -> None:
   book_path = 'books/frankenstein.txt'
   text = get_book_text(book_path)
@@ -8,9 +10,5 @@ def main() -> None:
 def get_book_text(filepath: str) -> str:
   with open(filepath) as f:
     return f.read()
-
-def count_num_words(book_text: str) -> int:
-  temp = book_text.split()
-  return len(temp)
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_num_words(book_text: str) -> int:
+  temp = book_text.split()
+  return len(temp)
